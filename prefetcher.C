b4aa3dbff4f23441a74710dd069d511913af54a4
Brianna Fidder <bfidder@mines.edu>
2015-02-03 17:24:56 -0700

    Initial commit

---
+++ prefetcher.C
@@ -0,0 +1,24 @@
+#include "prefetcher.h"
+#include "mem-sim.h"
+#include <stdio.h>
+
+Prefetcher::Prefetcher() {_ready = false;}
+
+bool Prefetcher::hasRequest(u_int32_t cycle) {
+	return _ready;
+}
+
+Request Prefetcher::getRequest(u_int32_t cycle) {
+	//Request req;
+	//return req;
+	return _nextReq;
+}
+
+void Prefetcher::completeRequest(u_int32_t cycle) { _ready = false; }
+
+void Prefetcher::cpuRequest(Request req) { 
+	if(!_ready && !req.HitL1) {
+		_nextReq.addr = req.addr + 16;
+		_ready = true;
+	}
+}
